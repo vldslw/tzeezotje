67933da292895ce28220a315d5583aff4a5a5909
Vladislav Pavlov <vldslw@gmail.com>
2023-07-31 20:16:19 -0300

    feat: add home page structure

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,15 @@ import './index.css';
 
 export const HomePage = () => {
 	return (
-		<main className="main">
-		</main>
+		<div className="page">
+			<header className="header">
+			</header>
+			<main className="content">
+			  <section className="menu" aria-label="Menu section">
+				</section>
+				<section className="clients" aria-label="Client reviews section">
+				</section>
+			</main>
+		</div>
 	);
 };
